af2b4d3471e4650d6ab3b47a576711aac4e28204
Malcolm Nesbeth <mnesbeth@protonmail.ch>
2019-12-18 00:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// write your code below!
+                  // write your code below!
+
+
+function happyHolidays(string){
+  return ("Happy Holidays!");
+}
+
+
+function happyHolidaysTo()
+{
+  return string(`Happy holidays, ${name}`)
+}
+
+happyHolidaysTo('a')
+happyHolidaysTo('b')
+happyHolidaysTo('c')
+
+console.log('a')
